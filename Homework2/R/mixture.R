c164794fbde7cbef938bb7f24b74573ce82d5bb1
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-11-19 16:32:34 -0500

    Add first draft of newton.mix

--- Homework2/R/mixture.R
+++ Homework2/R/mixture.R
@@ -1,14 +1,36 @@
-mixture <- function(y, method = c("newton", "EM"), param0 = NULL) {
+mixture <- function(y, method = c("newton", "EM"), maxit, tol = 1e-8, param0 = NULL) {
+        stopifnot(maxit > 0)
         method <- match.arg(method)
+        maxit <- as.integer(maxit)
         switch(method,
-               newton = newton.mix(y, param0),
-               EM = em.mix(y, param0))
+               newton = newton.mix(y, param0, maxit, tol),
+               EM = em.mix(y, param0, maxit))
 }
 
-newton.mix <- function(y, param0) {
-        
+newton.mix <- function(y, param0, maxit, tol) {
+        grad <- deriv3(~ log(lambda * dnorm((x - m1)/s1)/s1 + (1-lambda) * dnorm((x-m2)/s2)/s2), c("lambda", "m1", "m2", "s1", "s2"), c("y", "lambda", "m1", "m2", "s1", "s2"))
+        drv <- function(y, param) {
+                g <- grad(y, param[1], param[2], param[3], param[4], param[5])
+                list(gradient = colSums(attr(g, "gradient")),
+                     hessian = colSums(attr(g, "hessian"), dims = 1))
+        }
+        for(i in seq_len(maxit)) {
+                d <- drv(y, param0)
+                grad <- d$gradient
+                hess <- d$hessian
+                param <- solve(hess, hess %*% param0 - grad)
+                delta <- abs(param - param0)
+                if(delta < tol) {
+                        convergence <- 0
+                        break
+                }
+        }
+        if(i == maxit && delta >= tol)
+                convergence <- 1
+        hess <- drv(y, param)$hessian
+        list(mle = param, stderr = sqrt(diag(hess)), convergence = convergence)
 }
 
-em.mix <- function(y, param0) {
+em.mix <- function(y, param0, maxit, tol) {
 
 }
