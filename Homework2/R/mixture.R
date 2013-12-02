65215eccb674df387953ad344c75252a480e2ad0
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-12-01 22:07:56 -0500

    Add Louis's method for standard errors

--- Homework2/R/mixture.R
+++ Homework2/R/mixture.R
@@ -3,11 +3,13 @@ mixture <- function(y, method = c("newton", "EM"), maxit = NULL, tol = 1e-8,
         method <- match.arg(method)
         switch(method,
                newton = {
-                       maxit <- 100
+                       if(is.null(maxit))
+                               maxit <- 100
                        newton.mix(y, param0, maxit, tol)
                },
                EM = {
-                       maxit <- 500
+                       if(is.null(maxit))
+                               maxit <- 500
                        em.mix(y, param0, maxit, tol)
                })
 }
@@ -18,23 +20,24 @@ newton.mix <- function(y, param0, maxit, tol) {
                 param0 <- c(lambda = 0.5, m1 = sample(y, 1),
                             m2 = sample(y, 1), s1 = sd(y)/2, s2 = sd(y)/2)
         }
-        gradfun <- deriv3(~ log(lambda * dnorm((y - m1)/s1)/s1 + (1-lambda) * dnorm((y-m2)/s2)/s2), c("lambda", "m1", "m2", "s1", "s2"), c("y", "lambda", "m1", "m2", "s1", "s2"))
-        drv <- function(y, param) {
-                g <- gradfun(y, param[1], param[2], param[3],
-                             param[4], param[5])
-                list(likelihood = sum(g),
-                     gradient = colSums(attr(g, "gradient")),
-                     hessian = colSums(attr(g, "hessian"), dims = 1))
-        }
-        d <- drv(y, param0)
+        d <- drv(y, param0, gradfun)
         ll0 <- d$likelihood
         grad <- d$gradient
         hess <- d$hessian
+        alpha <- 1
+        delta0 <- Inf
         for(i in seq_len(maxit)) {
-                param <- drop(solve(hess, hess %*% param0 - grad))
-                d <- drv(y, param)
+                param <- drop(solve(hess, hess %*% param0 - grad * alpha))
+                d <- drv(y, param, gradfun)
                 ll <- d$likelihood
-                delta <- abs(ll - ll0)
+                ## delta <- abs(ll - ll0)
+                delta <- sqrt(sum((param - param0)^2))
+                cat(i, "delta:", delta, "\tLL:", ll, "\n")
+                if(delta > delta0) {
+                        alpha <- 0.95 * alpha
+                        message(sprintf("resetting step length: %f", alpha))
+                        next
+                }
                 if(delta < tol) {
                         convergence <- 0
                         break
@@ -43,12 +46,23 @@ newton.mix <- function(y, param0, maxit, tol) {
                         ll0 <- ll
                         grad <- d$gradient
                         hess <- d$hessian
+                        delta0 <- delta
                 }
         }
         if(i == maxit && delta >= tol)
                 convergence <- 1
-        vcov <- solve(hess)
+        browser()
+        vcov <- -solve(hess)
         list(mle = param, stderr = sqrt(diag(vcov)), convergence = convergence)
+}
+
+gradfun <- deriv3(~ log(lambda * dnorm((y - m1)/s1)/s1 + (1-lambda) * dnorm((y-m2)/s2)/s2), c("lambda", "m1", "m2", "s1", "s2"), c("y", "lambda", "m1", "m2", "s1", "s2"))
+
+drv <- function(y, param, gradfun) {
+        g <- gradfun(y, param[1], param[2], param[3], param[4], param[5])
+        list(likelihood = sum(g),
+             gradient = colSums(attr(g, "gradient")),
+             hessian = colSums(attr(g, "hessian"), dims = 1))
 }
 
 em.mix <- function(y, param0, maxit, tol) {
@@ -67,7 +81,6 @@ em.mix <- function(y, param0, maxit, tol) {
                 s2 <- with(plist, sqrt(sum((1-z) * (y-m2)*(y-m2)) / (sum(1-z))))
                 param <- c(lambda = lambda, m1 = m1, m2 = m2, s1 = s1, s2 = s2)
                 delta <- sqrt(sum((param - param0)^2))
-                cat(i, "delta:", delta, "\n")
                 if(delta < tol) {
                         convergence <- 0
                         break
@@ -79,10 +92,22 @@ em.mix <- function(y, param0, maxit, tol) {
         if(i == maxit && delta >= tol)
                 convergence <- 1
         stderr <- compute.stderr(y, param)
-        list(mle = param, stderr = stderr, convergence = convergence)
+        list(mle = param, stderr = stderr, convergence = convergence,
+             iterations = i)
 }
 
 ## Compute standard errors using Louis's method
 compute.stderr <- function(y, param) {
-        rep(1, length(param))
+        plist <- as.list(param)
+        z <- with(plist, (lambda * dnorm(y, m1, s1)) / (lambda*dnorm(y,m1,s1)+(1-lambda)*dnorm(y,m2,s2)))
+        S <- with(plist, {
+                cbind(lambda = z / lambda - (1 - z) / (1 - lambda),
+                      m1 = z * (y - m1) / s1^2,
+                      m2 = (1 - z) * (y - m2) / s2^2,
+                      s1 = z * (-1/s1 + (y - m1)^2 / s1^3),
+                      s2 = (1 - z) * (-1/s2 + (y - m2)^2 / s2^3))
+        })
+        I <- crossprod(S)
+        H <- solve(I)
+        sqrt(diag(H))
 }
